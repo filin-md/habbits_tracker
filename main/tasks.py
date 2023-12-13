3e1436409dde3a1c6bf388e53d7e6380a1102359
filin-md <filin.md9@gmail.com>
2023-12-13 15:32:48 +0300

    change service function

--- main/tasks.py
+++ main/tasks.py
@@ -16,20 +16,21 @@ def habit_notification():
             days_since_last_sent = (now.date() - habit.last_sent_date).days if habit.last_sent_date else (now.date() - habit.created_at).days
             message = f"Нужно выполнить {habit.action} в {habit.place} в {habit.time}"
 
-            if days_since_last_sent >= 1:
-
+            if habit.frequency == 'daily':
+                if days_since_last_sent >= 1:
+                    send_telegram_msg(habit.owner.tg_username, message)
+                    habit.last_sent_date = now.date()
+                    habit.save()
+                    return
+            elif habit.frequency == 'weekly':
                 if days_since_last_sent >= 7:
                     send_telegram_msg(habit.owner.tg_username, message)
                     habit.last_sent_date = now.date()
                     habit.save()
                     return
+            elif habit.frequency == 'monthly':
                 if days_since_last_sent >= 30:
                     send_telegram_msg(habit.owner.tg_username, message)
                     habit.last_sent_date = now.date()
                     habit.save()
                     return
-
-                send_telegram_msg(habit.owner.tg_username, message)
-                habit.last_sent_date = now.date()
-                habit.save()
-                return
